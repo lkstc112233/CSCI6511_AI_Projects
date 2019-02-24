4c2f6741910199acbaf190a03f66755684fd9b8a
Photon cat <lkstc112233@users.noreply.github.com>
2019-02-24 02:39:41 -0500

    Added storage support for adapter and gameid.

--- src/com/photoncat/aiproj2/game/Game.java
+++ src/com/photoncat/aiproj2/game/Game.java
@@ -6,6 +6,27 @@ import com.photoncat.aiproj2.io.Adapter;
  * This is the gaming thread. Each game is handled in a separate thread to support functions of polling.
  */
 public class Game extends Thread{
+    private Adapter ioAdapter;
+    private int gameId;
+
+    /**
+     * Host a game with another team.
+     */
+    public Game(Adapter ioAdapter, int otherTeamId, int boardSize, int target) {
+        this.ioAdapter = ioAdapter;
+        ioAdapter.createGame(otherTeamId, boardSize, target);
+    }
+
+    /**
+     * Join another game.
+     * @param ioAdapter
+     * @param gameId
+     */
+    public Game(Adapter ioAdapter, int gameId) {
+        this.ioAdapter = ioAdapter;
+        this.gameId = gameId;
+    }
+
     @Override
     public void run() {}
 }
