02eeccb5d50c5b1a42da5590813aa01733320dea
Photon cat <lkstc112233@users.noreply.github.com>
2019-02-24 02:33:49 -0500

    Update comments

--- src/com/photoncat/aiproj2/game/Game.java
+++ src/com/photoncat/aiproj2/game/Game.java
@@ -2,6 +2,9 @@ package com.photoncat.aiproj2.game;
 
 import com.photoncat.aiproj2.io.Adapter;
 
+/**
+ * This is the gaming thread. Each game is handled in a separate thread to support functions of polling.
+ */
 public class Game extends Thread{
     @Override
     public void run() {}
