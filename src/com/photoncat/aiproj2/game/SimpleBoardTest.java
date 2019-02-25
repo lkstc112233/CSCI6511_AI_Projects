0976be9f34b2f08740cef23bd90b360f9012e37c
Photon cat <lkstc112233@users.noreply.github.com>
2019-02-25 02:37:24 -0500

    Added test for taking a step back.

--- src/com/photoncat/aiproj2/game/SimpleBoardTest.java
+++ src/com/photoncat/aiproj2/game/SimpleBoardTest.java
@@ -6,6 +6,7 @@ public class SimpleBoardTest {
     public static void main(String[] args) {
         test3x3();
         test5x5();
+        testTakeback();
         System.out.println("All test passed");
     }
 
@@ -28,6 +29,53 @@ public class SimpleBoardTest {
         if (simpleBoard.gameover()) throw new AssertionError();
         if (!simpleBoard.putPiece(2, 2)) throw new AssertionError(); // O, wins
         if (simpleBoard.wins() != Board.PieceType.CIRCLE) throw new AssertionError();
+        if (!simpleBoard.gameover()) throw new AssertionError();
+    }
+
+    private static void testTakeback() {
+        // Test based on a 3x3 board. aka classic tic-tac-toe
+        Board simpleBoard = new SimpleBoard(3,3);
+        if (!simpleBoard.putPiece(1, 1)) throw new AssertionError(); // O
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        // Cannot replace any placed position.
+        if (simpleBoard.putPiece(1, 1)) throw new AssertionError();  // X
+        if (!simpleBoard.putPiece(0, 1)) throw new AssertionError(); // X
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        // Takes back last step
+        simpleBoard.takeBack();
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        // Place on a different spot
+        if (!simpleBoard.putPiece(1, 0)) throw new AssertionError(); // X
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        // O now can be put on the last spot.
+        if (!simpleBoard.putPiece(0, 1)) throw new AssertionError(); // O
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(1, 2)) throw new AssertionError(); // X
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(2, 1)) throw new AssertionError(); // O, wins
+        if (simpleBoard.wins() != Board.PieceType.CIRCLE) throw new AssertionError();
+        if (!simpleBoard.gameover()) throw new AssertionError();
+        // Now take that back
+        simpleBoard.takeBack();
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(2, 2)) throw new AssertionError(); // O
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(0, 0)) throw new AssertionError(); // X
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(0, 2)) throw new AssertionError(); // O
+        if (simpleBoard.wins() != Board.PieceType.NONE) throw new AssertionError();
+        if (simpleBoard.gameover()) throw new AssertionError();
+        if (!simpleBoard.putPiece(2, 0)) throw new AssertionError(); // X, wins
+        if (simpleBoard.wins() != Board.PieceType.CROSS) throw new AssertionError();
         if (!simpleBoard.gameover()) throw new AssertionError();
     }
 
