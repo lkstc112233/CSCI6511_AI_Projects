b04ecb60395af7e00ce38c306650dedfc7efa675
Photon cat <lkstc112233@users.noreply.github.com>
2019-02-26 00:13:04 -0500

    Implemented the checking for whose turn is it.

--- src/com/photoncat/aiproj2/game/Game.java
+++ src/com/photoncat/aiproj2/game/Game.java
@@ -7,6 +7,7 @@ import com.photoncat.aiproj2.io.Adapter;
  * This is the gaming thread. Each game is handled in a separate thread to support functions of polling.
  */
 public class Game extends Thread{
+    private static final int POLLING_INTERVAL_MILLISECONDS = 5000;
     private Adapter ioAdapter;
     private int gameId;
 
@@ -34,6 +35,17 @@ public class Game extends Thread{
             int x = 0;
             int y = 0;
             ioAdapter.moveAt(gameId, x, y);
+            while (ioAdapter.getLastMove(gameId) == Board.PieceType.CROSS) {
+                // Wait for 5 seconds - As Professor Arora suggested in slack.
+                try {
+                    Thread.sleep(POLLING_INTERVAL_MILLISECONDS);
+                } catch (InterruptedException e) {
+                    // Ignore the interrupted since:
+                    // a) There won't be any interruption.
+                    // b) Even if there were any, we can simply recheck the status.
+                    e.printStackTrace();
+                }
+            }
             do {
                 board = ioAdapter.getBoard(gameId);
             } while (board == null);
