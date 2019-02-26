d77f6f77306972bdf7ddcef9974001ca0f8246b3
Photon cat <lkstc112233@users.noreply.github.com>
2019-02-25 23:42:45 -0500

    Implemented console adapter behavior

--- src/com/photoncat/aiproj2/io/ConsoleAdapter.java
+++ src/com/photoncat/aiproj2/io/ConsoleAdapter.java
@@ -1,10 +1,12 @@
 package com.photoncat.aiproj2.io;
 
+import com.photoncat.aiproj2.game.SimpleBoard;
 import com.photoncat.aiproj2.interfaces.Board;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * This is a console adapter for development.
@@ -13,21 +15,32 @@ public class ConsoleAdapter implements Adapter{
     Map<Integer, Board> games = new HashMap<>();
     @Override
     public int createGame(int otherTeamId, int boardSize, int target) {
+        games.put(0, new SimpleBoard(boardSize, target));
         return 0;
     }
 
     @Override
     public void moveAt(int gameId, int x, int y) {
-
+        games.get(gameId).putPiece(x, y);
     }
 
     @Override
     public Board.PieceType getLastMove(int gameId) {
-        return null;
+        return Board.PieceType.CROSS;
     }
 
     @Override
     public Board getBoard(int gameId) {
-        return null;
+        // Get console input.
+        var board = games.get(gameId);
+        System.out.println(board.toString());
+        System.out.println("Input x and y: ");
+        int x;
+        int y;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        board.putPiece(x, y);
+        return board;
     }
 }
