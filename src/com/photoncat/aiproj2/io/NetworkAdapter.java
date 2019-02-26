30fa3d22ade1e759080a519d8c5331b47116c536
Photon Cat <lkstc112233@users.noreply.github.com>
2019-02-26 17:39:18 -0500

    Added skeleton of the Network adapter

---
+++ src/com/photoncat/aiproj2/io/NetworkAdapter.java
@@ -0,0 +1,15 @@
+package com.photoncat.aiproj2.io;
+
+/**
+ * The adapter to the p2p gaming server api.
+ * http://www.notexponential.com/aip2pgaming/api/index.php
+ *
+ * The document of the API can be found at:
+ * https://docs.google.com/presentation/d/1apI8JBF4FK8nm1xFmj0ud4O51VAJnjkiZxmbIeBqADE/edit#slide=id.g336cc86b3f_0_5
+ *
+ * Our team ID is 1102 and I'm hard wiring the ID into the code.
+ *
+ * The API requires an auth whose required information will be loaded from a file therefore won't be check in.
+ */
+public class NetworkAdapter {
+}
