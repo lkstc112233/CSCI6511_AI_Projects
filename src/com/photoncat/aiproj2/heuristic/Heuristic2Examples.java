ece043906f6f3fb8706a7086866c62021856dfe6
Photon cat <lkstc112233@users.noreply.github.com>
2019-03-22 01:12:20 -0400

    Updated the examples

--- src/com/photoncat/aiproj2/heuristic/Heuristic2Examples.java
+++ src/com/photoncat/aiproj2/heuristic/Heuristic2Examples.java
@@ -13,67 +13,49 @@ public class Heuristic2Examples {
         Heuristics heuristics = new Heuristic2();
         List<Board> boards = new ArrayList<>();
         LoadedBoard boardToAdd = new LoadedBoard(
-                "----------\n" +
-                "----------\n" +
-                "----------\n" +
-                "----X-----\n" +
-                "----OX----\n" +
-                "----O-----\n" +
-                "----------\n" +
-                "----O-----\n" +
-                "----------\n" +
-                "----------\n",5);
-        boardToAdd.setLastMove(new Move(7, 4), Board.PieceType.CIRCLE);
-        boards.add(boardToAdd);
-        boardToAdd = new LoadedBoard("----------\n" +
-                "----------\n" +
-                "----------\n" +
-                "----X-----\n" +
-                "----OX----\n" +
-                "----O-O---\n" +
-                "----------\n" +
-                "----------\n" +
-                "----------\n" +
-                "----------\n",5);
-        boardToAdd.setLastMove(new Move(5, 6), Board.PieceType.CIRCLE);
-        boards.add(boardToAdd);
-        boardToAdd = new LoadedBoard("----------\n" +
-                "----------\n" +
-                "----------\n" +
-                "----X-----\n" +
-                "----OX----\n" +
-                "---OO-----\n" +
-                "----------\n" +
-                "----------\n" +
-                "----------\n" +
-                "----------\n",5);
-        boardToAdd.setLastMove(new Move(5, 3), Board.PieceType.CIRCLE);
+                "------------\n" +
+                        "-----X------\n" +
+                        "-----O------\n" +
+                        "-----O------\n" +
+                        "-----OX-O---\n" +
+                        "-XOOOOOX----\n" +
+                        "-----OX-X---\n" +
+                        "-----XOXXX--\n" +
+                        "----X---X---\n" +
+                        "--------X--O\n" +
+                        "--O-----O---\n" +
+                        "------------\n",6);
+        boardToAdd.setLastMove(new Move(10, 8), Board.PieceType.CIRCLE);
         boards.add(boardToAdd);
         boardToAdd = new LoadedBoard(
-                "----------\n" +
-                        "----------\n" +
-                        "----------\n" +
-                        "----X-----\n" +
-                        "----OX----\n" +
-                        "----O-----\n" +
-                        "----O-----\n" +
-                        "----------\n" +
-                        "----------\n" +
-                        "----------\n",5);
-        boardToAdd.setLastMove(new Move(6, 4), Board.PieceType.CIRCLE);
+                "------------\n" +
+                        "-----X------\n" +
+                        "-----O------\n" +
+                        "-----O------\n" +
+                        "---O-OX-O---\n" +
+                        "-XOOOOOX----\n" +
+                        "-----OX-X---\n" +
+                        "-----XOXXX--\n" +
+                        "----X---X---\n" +
+                        "--------X--O\n" +
+                        "--O---------\n" +
+                        "------------\n",6);
+        boardToAdd.setLastMove(new Move(4, 3), Board.PieceType.CIRCLE);
         boards.add(boardToAdd);
         boardToAdd = new LoadedBoard(
-                "----------\n" +
-                        "----------\n" +
-                        "----------\n" +
-                        "----X-----\n" +
-                        "----OX----\n" +
-                        "----O-X---\n" +
-                        "----------\n" +
-                        "----O-----\n" +
-                        "----O-----\n" +
-                        "----------\n",5);
-        boardToAdd.setLastMove(new Move(8, 4), Board.PieceType.CIRCLE);
+                "------------\n" +
+                        "-----X------\n" +
+                        "-----O------\n" +
+                        "-----O------\n" +
+                        "-----OX-O---\n" +
+                        "-XOOOOOXO---\n" +
+                        "-----OX-X---\n" +
+                        "-----XOXXX--\n" +
+                        "----X---X---\n" +
+                        "--------X--O\n" +
+                        "--O---------\n" +
+                        "------------\n",6);
+        boardToAdd.setLastMove(new Move(5, 8), Board.PieceType.CIRCLE);
         boards.add(boardToAdd);
         for (Board board: boards) {
             System.out.println(board.toString());
